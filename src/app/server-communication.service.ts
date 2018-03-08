23fa8aad2b4cf7be7e4c3575b77740426bb31974
Dmitriytro <Dmitriytro@gmail.com>
2018-03-07 23:40:16 -0800

    remove some staff

--- src/app/server-communication.service.ts
+++ src/app/server-communication.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
+import { ZWSID } from "../../zwsid";
 
 @Injectable()
 export class ServerCommunicationService {
-  ZWSID: string = 'X1-ZWz18qazbt5onf_98wrk';
   serverUrl: string = '';
-  example: string = `http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${this.ZWSID}&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`;
+  key: string = ZWSID;
+  example: string = `http://www.zillow.com/webservice/GetSearchResults.htm?zws-id=${this.key}&address=2114+Bigelow+Ave&citystatezip=Seattle%2C+WA`;
   constructor(private httpClient: HttpClient) { }
   fetch(term: string){
     return this.httpClient.get(this.example)
