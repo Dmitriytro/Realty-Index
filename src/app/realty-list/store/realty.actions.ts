e0bfa93641323f84ba28e7db85a7eea6be3ea288
Dmitriytro <Dmitriytro@gmail.com>
2018-03-14 00:17:23 -0700

    type bug was fixed

--- src/app/realty-list/store/realty.actions.ts
+++ src/app/realty-list/store/realty.actions.ts
@@ -7,21 +7,21 @@ export const GET_REALTY = 'get_realty';
 export const SELECT_PROPERTY = 'select_property';
 
 export class SetParams implements Action {
-  readonly type: string = CONFIG_PARAMS;
+  readonly type = CONFIG_PARAMS;
   constructor(public payload: Params) {}
 }
 
 export class Select implements Action {
-  readonly type: string = SELECT_PROPERTY;
-  constructor(public payload: any) {}
+  readonly type = SELECT_PROPERTY;
+  constructor(public payload: number) {}
 }
 
 export class GetRealty implements Action {
-  readonly type: string = GET_REALTY;
+  readonly type = GET_REALTY;
   constructor(public payload: Realty[]) {}
 }
 
 export type Actions
-  = Select
+  = GetRealty
   | SetParams
-  | GetRealty;
+  | Select;
