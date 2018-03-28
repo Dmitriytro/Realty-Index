48c409a61f96ad419818f942514b6a6939ce0751
Dmitriytro <Dmitriytro@gmail.com>
2018-03-27 22:13:47 -0700

    flex box setted up

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,18 +1,15 @@
 import { NgModule } from '@angular/core';
-import { NavigationComponent } from './navigation/navigation.component';
 import { HomeComponent } from './home/home.component';
-import { NavigationTabsComponent } from './navigation/navigation-tabs/navigation-tabs.component';
 import { SharedModule } from '../shared/shared.module';
 import { AppRoutingModule } from '../app-routing/app-routing.module';
 import { FormsModule } from '@angular/forms';
 import { ServerCommunicationService } from '../server-communication.service';
 import { MaterialModule } from '../material/material.module';
 import { SidenavComponent } from './sidenav/sidenav.component';
+import { FlexLayoutModule } from '@angular/flex-layout';
 
 @NgModule({
   declarations: [
-    NavigationComponent,
-    NavigationTabsComponent,
     HomeComponent,
     SidenavComponent
   ],
@@ -20,14 +17,14 @@ import { SidenavComponent } from './sidenav/sidenav.component';
     SharedModule,
     AppRoutingModule,
     FormsModule,
-    MaterialModule
+    MaterialModule,
+    FlexLayoutModule
   ],
   providers: [
     ServerCommunicationService
   ],
   exports: [
     AppRoutingModule,
-    NavigationComponent,
     SidenavComponent
   ]
 })
--- src/app/core/sidenav/sidenav.component.ts
+++ src/app/core/sidenav/sidenav.component.ts
@@ -1,4 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+import * as firebase from 'firebase';
+import { Store } from '@ngrx/store';
+import * as AuthActions from '../../auth/store/auth.actions';
+import { AppState } from '../../store/app.reducer';
+import { NgForm } from '@angular/forms';
+import * as RealtyActions from '../../realty/store/realty.actions';
 
 @Component({
   selector: 'app-sidenav',
@@ -6,7 +13,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./sidenav.component.css']
 })
 export class SidenavComponent implements OnInit {
-  constructor() { }
+  authState: Observable<{token: string, isAuthenticated: boolean}>;
+  constructor(private store: Store<AppState>) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+    this.authState = this.store.select('authReducer');
+  }
+  onSignOut() {
+    firebase.auth().signOut();
+    this.store.dispatch(new AuthActions.OnSighOut());
+  }
+  search(searchInput: NgForm) {
+    this.store.dispatch(new RealtyActions.SetParams(searchInput.value));
+    searchInput.reset();
+  }
 }
